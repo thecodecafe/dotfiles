51be31daf4834c79e181ceda0b4bd228ba159c5d
Daniel Barde <danielbarde@Daniels-MacBook-Pro-13inch.local>
2026-08-29 08:56:12 +0100

    feat(nvim): browse files with Oil

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/oil.nvim",
+  lazy = false,
+  opts = {
+    default_file_explorer = true,
+  },
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
+}
